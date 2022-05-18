31c58957a0f5b2583759e31075d0d91c9e9bbbfb
Brayon França <brayonfr@gmail.com>
2022-05-18 15:36:26 -0300

    feat(users): code refactor

--- src/services/users/listUsers.service.ts
+++ src/services/users/listUsers.service.ts
@@ -5,18 +5,9 @@ const listUsersSrvice = async()=>{
     const repository = AppDataSource.getRepository(User)
 
     const users = await repository.find()
-    const usersReturn = users.map(users=>{
+    
+    const usersReturn = users.map(user=>({...user,password:undefined}))
 
-        const user ={
-            id:users.name,
-            name:users.name,
-            email: users.email,
-            description: users.description,
-            created_at: users.created_at,
-            updated_at:users.updated_at
-        }
-        return user
-    })
    
     return usersReturn
 }
