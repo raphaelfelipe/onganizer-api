752abe15e3ee5d102d1b84d606e9f8d1f5ecb412
Brayon França <brayonfr@gmail.com>
2022-05-18 14:50:28 -0300

    feat(users): removed password from return

--- src/services/users/listUsers.service.ts
+++ src/services/users/listUsers.service.ts
@@ -4,9 +4,21 @@ import { User } from "../../entities/user.entity"
 const listUsersSrvice = async()=>{
     const repository = AppDataSource.getRepository(User)
 
-    const users = repository.find()
+    const users = await repository.find()
+    const usersReturn = users.map(users=>{
 
-    return users
+        const user ={
+            id:users.name,
+            name:users.name,
+            email: users.email,
+            description: users.description,
+            created_at: users.created_at,
+            updated_at:users.updated_at
+        }
+        return user
+    })
+   
+    return usersReturn
 }
 
 export default listUsersSrvice 
