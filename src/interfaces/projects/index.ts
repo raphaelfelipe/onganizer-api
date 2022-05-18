996bf08a794ff345c143e603e3797f8ed7c97152
Raphael Felipe <macedolima22@hotmail.com-Aspire-E5-571.local>
2022-05-18 14:21:42 -0300

    feat: service_projectCreate done

---
+++ src/interfaces/projects/index.ts
@@ -0,0 +1,8 @@
+export interface IProjectCreate {
+  name: string;
+  description: string;
+  objective: string;
+  created_at: Date;
+  updated_at: Date;
+  active: boolean;
+}
---
+++ src/services/projects/projectCreate.service.ts
@@ -0,0 +1,34 @@
+import { Project } from "../../entities/project.entity";
+import { IProjectCreate } from "../../interfaces/projects";
+import { AppDataSource } from "../../data-source";
+
+const projectCreateService = async ({
+  name,
+  description,
+  objective,
+  created_at,
+  updated_at,
+  active,
+}: IProjectCreate) => {
+  const projectRepository = AppDataSource.getRepository(Project);
+  const projects = await projectRepository.find();
+  const projectAlreadyExists = projects.find(
+    (project) => project.name === name
+  );
+  if (projectAlreadyExists) {
+    throw new Error("Project already exists");
+  }
+  const project = new Project();
+  project.name = name;
+  project.description = description;
+  project.objective = objective;
+  project.created_at = new Date();
+  project.updated_at = new Date();
+  project.active = true;
+
+  projectRepository.create(project);
+  await projectRepository.save(project);
+  return project;
+};
+
+export default projectCreateService;
