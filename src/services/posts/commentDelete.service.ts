8b9e5302f3fb7022d6cf9b4a4e43316405d3c210
oswaldo <91742210+oswaldo-deco@users.noreply.github.com>
2022-05-19 14:30:47 -0300

    feat(service/posts): comment delete service was implemented

---
+++ src/services/posts/commentDelete.service.ts
@@ -0,0 +1,17 @@
+import { AppDataSource } from "../../data-source";
+import { Post_Comments } from "../../entities/post_comments.entity";
+import { ICommentId } from "../../interfaces/posts";
+
+const commentDeleteService = async ({id}: ICommentId) => {
+  const commentRepository = AppDataSource.getRepository(Post_Comments);
+
+  const comments = await commentRepository.find();
+
+  const commentDeleted = comments.find((comment) => comment.id === id);
+
+  await commentRepository.delete(commentDeleted!.id);
+
+  return true;
+};
+
+export default commentDeleteService;
