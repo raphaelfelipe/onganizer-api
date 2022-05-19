d2f7c94093c1bb16093841dddf7822fe2bc1740d
Carlos Vitor <limacarlosvitor@gmail.com>
2022-05-19 18:13:18 -0300

    feat: added routes for projects

---
+++ src/routes/project.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import ProjectsController from "../controllers/projects.controller"
+
+const router = Router()
+const projectsController = new ProjectsController()
+
+router.get("", projectsController.projectList)
+router.get("/:id", projectsController.projectListOne)
+router.get("/:id/users", projectsController.listProjectUsers)
+router.get("/:id/post", projectsController.listProjectPosts)
+router.post("", projectsController.createProject)
+router.post("/users", projectsController.createProjectUsers)
+router.post("/:id/posts", projectsController.createPost)
+router.post("/follow/:id", projectsController.createFollowProject)
+router.patch("/:id", projectsController.projectUpdate)
+router.delete("/:id", projectsController.projectDelete)
+router.delete("/follow/:id", projectsController.followProjectDelete)
+
+export default router
