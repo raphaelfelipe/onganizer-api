5d62f76fa7241658b0de1085578fcfafe3432fa4
Guilherme Milék <guilhermeprevedamilek@gmail.com>
2022-05-19 21:51:00 -0300

    feat(authuser.middleware.ts): create the authUser middleware

---
+++ src/middlewares/authUser.middleware.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+
+export const authProject = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id: userId } = req.params;
+
+    if (userId !== req.userId) {
+      throw new Error();
+    }
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      message: "You are not allowed to do that on this user",
+    });
+  }
+};
