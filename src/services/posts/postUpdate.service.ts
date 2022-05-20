efd5209b8ef078313a7b0845b39ab8ea77e3a49c
oswaldo <91742210+oswaldo-deco@users.noreply.github.com>
2022-05-20 10:08:44 -0300

    fix(postupdate.service): postUpdate service was recovered to the development branch

---
+++ src/services/posts/postUpdate.service.ts
@@ -0,0 +1,22 @@
+import { AppDataSource } from "../../data-source"
+import { Project_Posts } from "../../entities/project_posts.entity"
+import { IPostUpdate } from "../../interfaces/posts"
+
+const postUpdateService = async({id, title, content}:IPostUpdate)=>{
+
+    const postsRepository = AppDataSource.getRepository(Project_Posts)
+    const posts = await postsRepository.find()
+    const post = posts.find(post => post.id === id)
+  
+    await postsRepository.update(post!.id,{title: title, content: content})
+
+    return {
+        message:'Post successfully updated', 
+        UpdatedInfo:{
+            title:title,
+            content: content, 
+        }
+    } 
+}
+
+export default postUpdateService
