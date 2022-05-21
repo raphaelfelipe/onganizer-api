b83a24720914918d6dfdf5e345434d0cac96b7f2
Raphael Felipe <macedolima22@hotmail.com-Aspire-E5-571.local>
2022-05-21 00:06:16 -0300

    fix: debuggin data-source and .env

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,14 +5,21 @@ require("dotenv").config();
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
-  ssl: process.env.NODE_END === "production" ? 
-    {rejectUnauthorized: false}
-    :false
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 
   synchronize: false,
   logging: true,
-  entities: process.env.NODE_ENV === "production" ? ["dist/entities/*.js"] : ["src/entities/*.ts"],
-  migrations: process.env.NODE_ENV === "production" ? ["dist/migrations/*.js"] : ["src/migrations/*.ts"],
+  entities:
+    process.env.NODE_ENV === "production"
+      ? ["dist/entities/*.js"]
+      : ["src/entities/*.ts"],
+  migrations:
+    process.env.NODE_ENV === "production"
+      ? ["dist/migrations/*.js"]
+      : ["src/migrations/*.ts"],
 });
 
 AppDataSource.initialize()
