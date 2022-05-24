7fa63f299d0b121dd359f2e016926407c6c2e9a9
oswaldo <91742210+oswaldo-deco@users.noreply.github.com>
2022-05-24 15:34:35 -0300

    feat(posts arquives): implemented posts middleware and refatored posts services

--- src/migrations/1653416368577-initialMigration.ts
+++ src/migrations/1653416368577-initialMigration.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class initialMigration1653398176187 implements MigrationInterface {
+export class initialMigration1653416368577 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
     }
--- src/services/projects/listProjectPosts.service.ts
+++ src/services/projects/listProjectPosts.service.ts
@@ -1,13 +1,14 @@
 import { Project_Posts } from "../../entities/project_posts.entity";
 import { AppDataSource } from "../../data-source";
 import { IProjectId } from "../../interfaces/projects";
+import { Project } from "../../entities/project.entity";
 
 const projectPostsService = async ({id} : IProjectId) => {
     const projectPostsRepository = AppDataSource.getRepository(Project_Posts)
 
-    const projectPosts = await projectPostsRepository.find()
-
-    const posts = projectPosts.filter((post)=>post.project_id === id)
+    const projectPosts = await projectPostsRepository.find({relations:["project"]})
+    console.log(id,projectPosts)
+    const posts = projectPosts.filter((post)=>post.project.id === id)
 
     return posts
 }
