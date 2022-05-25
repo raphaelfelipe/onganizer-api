101a909b7d4e9a9ab9bb369188fabfccc273b937
Raphael Felipe <macedolima22@hotmail.com-Aspire-E5-571.local>
2022-05-24 23:14:01 -0300

    fix: updateUser service updating password correctly

--- src/migrations/1653443818341-initialMigration.ts
+++ src/migrations/1653443818341-initialMigration.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class initialMigration1653436163354 implements MigrationInterface {
+export class initialMigration1653443818341 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
     }
