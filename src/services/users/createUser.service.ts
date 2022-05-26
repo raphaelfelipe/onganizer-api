32e4300f463c5bcdda7b410fc311e3c4c551b813
Raphael Felipe <macedolima22@hotmail.com-Aspire-E5-571.local>
2022-05-25 21:40:14 -0300

    fix: debuggin create user service loopping

--- src/services/users/createUser.service.ts
+++ src/services/users/createUser.service.ts
@@ -19,6 +19,14 @@ const userCreateService = async ({
     throw new AppError("Email already in use", 409);
   }
 
+  if (!name || !email || !password) {
+    throw new AppError("Missing name, email or password", 422);
+  }
+
+  if (!description) {
+    description = "";
+  }
+
   const user = new User();
   user.name = name;
   user.email = email;
