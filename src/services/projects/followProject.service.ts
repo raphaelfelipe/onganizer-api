41c297352f5f29de5b14bda7be4da2d7bfc17c40
Carlos Vitor <limacarlosvitor@gmail.com>
2022-05-26 11:47:47 -0300

    fix: service follow project

--- src/services/projects/followProject.service.ts
+++ src/services/projects/followProject.service.ts
@@ -2,6 +2,7 @@ import { Follow_Projects } from "../../entities/followed_projects.entity";
 import { IFollowCreate } from "../../interfaces/projects";
 import { AppDataSource } from "../../data-source";
 import { AppError } from "../../errors/appError";
+import { Project } from "../../entities/project.entity";
 
 const followProjectService = async ({ project_id, user_id }: IFollowCreate) => {
     const followsRepository = AppDataSource.getRepository(Follow_Projects)
@@ -10,10 +11,16 @@ const followProjectService = async ({ project_id, user_id }: IFollowCreate) => {
     })
     const returnFollow = follows.find(element => element.project_id === project_id)
 
-    console.log("teste", returnFollow)
-
     if (returnFollow) {
         throw new AppError("is already following this project", 404);
+    }
+
+    const projectRepository = AppDataSource.getRepository(Project)
+    const projects = await projectRepository.find()
+    const returnproject = projects.find(element => element.id === project_id)
+
+    if (!returnproject) {
+        throw new AppError("Project not found", 404);
     }
 
     const follow = new Follow_Projects()
