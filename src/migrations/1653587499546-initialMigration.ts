47d67a7a3db7562169b74b9a1678bd4ebf801a6c
Carlos Vitor <limacarlosvitor@gmail.com>
2022-05-26 15:03:50 -0300

    fix: entity project post

--- src/migrations/1653587499546-initialMigration.ts
+++ src/migrations/1653587499546-initialMigration.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class initialMigration1653520538164 implements MigrationInterface {
+export class initialMigration1653587499546 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
     }
