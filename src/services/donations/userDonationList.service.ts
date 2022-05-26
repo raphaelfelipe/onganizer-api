9775d90cbb0ffe7221a96c406233f294ce944c01
Carlos Vitor <limacarlosvitor@gmail.com>
2022-05-26 15:15:22 -0300

    fix: serivce donation list user

--- src/services/donations/userDonationList.service.ts
+++ src/services/donations/userDonationList.service.ts
@@ -6,9 +6,7 @@ import { AppError } from "../../errors/appError"
 const userDonationsService = async ({ user_id }: IDonationUserId) => {
     const donationsRepository = AppDataSource.getRepository(Donation)
 
-    const donations = await donationsRepository.find({
-        relations: ["user", "project"]
-    })
+    const donations = await donationsRepository.find()
 
     const userDonations = donations.filter((donation) => donation.user_id === user_id)
 
