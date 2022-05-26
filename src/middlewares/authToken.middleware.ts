f52a4709e85287dec8c4a493b8870c88f5702c3d
Raphael Felipe <macedolima22@hotmail.com-Aspire-E5-571.local>
2022-05-25 22:31:26 -0300

    fix: middleware authtoken updated

--- src/middlewares/authToken.middleware.ts
+++ src/middlewares/authToken.middleware.ts
@@ -1,27 +1,35 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { AppError, handleError } from "../errors/appError";
-
-export const authToken = (
+import { User } from "../entities/user.entity";
+import { AppDataSource } from "../data-source";
+export const authToken = async (
   request: Request,
   response: Response,
   next: NextFunction
 ) => {
   try {
+    const userRepository = AppDataSource.getRepository(User);
+    const users = await userRepository.find();
     const token = request.headers.authorization;
-
     jwt.verify(
       token as string,
       process.env.JWT_SECRET as string,
       (err: any, decoded: any) => {
-        if (!err) {
+        if (err) {
+          throw new AppError("Invalid token", 401);
+        }
+        const userFound = users.find(
+          (user) => user.email === decoded.email && user.id === decoded.id
+        );
+        if (userFound) {
           request.userEmail = decoded.email;
           request.userId = decoded.id;
           return next();
         }
+        throw new AppError("Invalid token", 401);
       }
     );
-    throw new AppError("Invalid token", 401);
   } catch (error) {
     if (error instanceof AppError) {
       handleError(error, response);
