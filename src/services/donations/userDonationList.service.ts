59acafc88c4a9ac85d3548a223ae89428926ef3a
Carlos Vitor <limacarlosvitor@gmail.com>
2022-05-26 15:56:10 -0300

    fix: service list user donation

--- src/services/donations/userDonationList.service.ts
+++ src/services/donations/userDonationList.service.ts
@@ -5,10 +5,8 @@ import { AppError } from "../../errors/appError"
 
 const userDonationsService = async ({ user_id }: IDonationUserId) => {
     const donationsRepository = AppDataSource.getRepository(Donation)
-
     const donations = await donationsRepository.find()
-
-    const userDonations = donations.filter((donation) => donation.user_id === user_id)
+    const userDonations = donations.filter(donation => donation.user_id === user_id)
 
     if (!userDonations) {
         throw new AppError("User donations not found", 404)
